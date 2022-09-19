eb9611eefa59660574d1653eacf3d62db8272320
Raphael <raphaellfpg@gmail.com>
2022-09-19 16:29:34 -0300

    Req 3 ICar generica

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 const VehicleSchema = z.object({
   model: z.string().min(3),
-  year: z.number(),
+  year: z.number().int(),
   color: z.string().min(3),
   status: z.boolean().optional(),
   buyValue: z.number().int(),
